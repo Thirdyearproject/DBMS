c0dbf66c3fbc1c59342149cac0d3e0a6b7dbe128
shrey <shrey23kumbhare@gmail.com>
2024-03-29 15:41:13 +0530

    seprate files created for table and triggers

---
+++ createTables.js
@@ -0,0 +1,128 @@
+// Import the MySQL connection pool
+const pool = require('./mysqlConnection'); // Adjust the path as needed
+
+// Function to create tables
+function createTables() {
+    // Create contacts table
+const createContactsTable = () => {
+    pool.query(
+      `CREATE TABLE IF NOT EXISTS contacts (
+          contact_id INT AUTO_INCREMENT PRIMARY KEY,
+          name VARCHAR(100) NOT NULL,
+          organization VARCHAR(100),
+          job_title VARCHAR(100),
+          date_of_birth DATE,
+          website_url VARCHAR(255),
+          notes TEXT,
+          tags VARCHAR(255)
+      )`,
+      (error, results, fields) => {
+        if (error) {
+          console.error("Error creating contacts table:", error);
+        } else {
+          console.log("Contacts table created or already exists");
+        }
+      }
+    );
+  };
+  // Create addresses table
+  const createAddressesTable = () => {
+    pool.query(
+      `CREATE TABLE IF NOT EXISTS addresses (
+          address_id INT AUTO_INCREMENT PRIMARY KEY,
+          Address_contact_id INT,
+          locality VARCHAR(100),
+          city VARCHAR(100),
+          state VARCHAR(100),
+          pin_code VARCHAR(20),     
+          FOREIGN KEY (Address_contact_id) REFERENCES contacts(contact_id)
+      )`,
+      (error, results, fields) => {
+        if (error) {
+          console.error("Error creating addresses table:", error);
+        } else {
+          console.log("Addresses table created or already exists");
+        }
+      }
+    );
+  };
+  
+  // Create phone_numbers table
+  const createPhoneNumbersTable = () => {
+    pool.query(
+      `CREATE TABLE IF NOT EXISTS phone_numbers (
+          phone_number_id INT AUTO_INCREMENT PRIMARY KEY,
+          phone_contact_id INT,
+          phone_number1 VARCHAR(15)  ,
+          phone_type1 VARCHAR(50),
+          phone_number2 VARCHAR(15)  ,
+          phone_type2 VARCHAR(50),
+          phone_number3 VARCHAR(15)  ,
+          phone_type3 VARCHAR(50),
+          FOREIGN KEY (phone_contact_id) REFERENCES contacts(contact_id)
+      )`,
+      (error, results, fields) => {
+        if (error) {
+          console.error("Error creating phone_numbers table:", error);
+        } else {
+          console.log("Phone Numbers table created or already exists");
+        }
+      }
+    );
+  };
+  
+  // Create emails table
+  const createEmailsTable = () => {
+    pool.query(
+      `CREATE TABLE IF NOT EXISTS emails (
+          email_id INT AUTO_INCREMENT PRIMARY KEY,
+          email_contact_id INT,
+          email_address1 VARCHAR(100)  ,
+          email_type1 VARCHAR(50),
+          email_address2 VARCHAR(100)  ,
+          email_type2 VARCHAR(50),
+          email_address3 VARCHAR(100)  ,
+          email_type3 VARCHAR(50),
+          FOREIGN KEY (email_contact_id) REFERENCES contacts(contact_id)
+      )`,
+      (error, results, fields) => {
+        if (error) {
+          console.error("Error creating emails table:", error);
+        } else {
+          console.log("Emails table created or already exists");
+        }
+      }
+    );
+  };
+  
+  // Create relationships table
+  const createRelationshipsTable = () => {
+    pool.query(
+      `CREATE TABLE IF NOT EXISTS relationships (
+          id INT AUTO_INCREMENT PRIMARY KEY,
+          person_id INT,
+          relationship_type VARCHAR(100),
+          FOREIGN KEY (person_id) REFERENCES contacts(contact_id)
+      )`,
+      (error, results, fields) => {
+        if (error) {
+          console.error("Error creating relationships table:", error);
+        } else {
+          console.log("Relationships table created or already exists");
+        }
+      }
+    );
+  };
+  
+  // Initialize tables
+  const initializeTables = () => {
+    createContactsTable();
+    createRelationshipsTable();
+    createAddressesTable();
+    createPhoneNumbersTable();
+    createEmailsTable();
+  };
+  initializeTables();
+  
+}
+module.exports = createTables;
---
+++ mysqlConnection.js
@@ -0,0 +1,13 @@
+const mysql = require("mysql2");
+
+// MySQL connection configuration
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  });
+
+// Export the MySQL connection object
+module.exports = pool;
