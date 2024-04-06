32d0c70175fd1db0d4f06536ea0ee88807cc3146
shrey <shrey23kumbhare@gmail.com>
2024-04-07 00:22:13 +0530

    filter for contacts visible to all

--- app.js
+++ app.js
@@ -1083,6 +1083,7 @@ app.get("/relationship/:personA/:personC", (req, res) => {
     });
 });
 
+//basic filter
 app.get('/filter', (req, res) => {
   const { searchQuery, city, dateOfBirthStart, dateOfBirthEnd, organization, job, relation } = req.query;
 
@@ -1102,6 +1103,60 @@ app.get('/filter', (req, res) => {
   if (city) {
     query += ' AND LOWER(a.city) LIKE ?'; // Changed to LIKE
     queryParams.push('%' + city.toLowerCase() + '%'); // Removed % wildcard characters
+  }
+
+  if (dateOfBirthStart && dateOfBirthEnd) {
+    query += ' AND c.date_of_birth BETWEEN ? AND ?';
+    queryParams.push(dateOfBirthStart, dateOfBirthEnd);
+  }
+
+  if (organization) {
+    query += ' AND LOWER(c.organization) LIKE ?';
+    queryParams.push('%' + organization.toLowerCase() + '%');
+  }
+
+  if (job) {
+    query += ' AND LOWER(c.job_title) LIKE ?';
+    queryParams.push('%' + job.toLowerCase() + '%');
+  }
+
+  console.log(query);
+  console.log(queryParams);
+
+  pool.query(query, queryParams, (error, results) => {
+    if (error) {
+      console.error('Error executing MySQL query:', error);
+      res.status(500).json({ error: 'Internal server error' });
+      return;
+    }
+    if (results.length === 0) { // Check if no contacts found
+      res.status(404).json({ message: 'No contacts found based on the provided criteria' });
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+//show filter contact visible to all 
+app.get('/filter_Visible_to_all', (req, res) => {
+  const { searchQuery, city, dateOfBirthStart, dateOfBirthEnd, organization, job, relation } = req.query;
+
+  let query = `
+    SELECT c.*
+    FROM contacts c
+    INNER JOIN addresses a ON c.contact_id = a.Address_contact_id
+    WHERE c.visible_to_all = 1
+  `;
+  let queryParams = [];
+
+  if (searchQuery) {
+    query += ' AND LOWER(c.name) LIKE ?';
+    queryParams.push('%' + searchQuery.toLowerCase() + '%');
+  }
+
+  if (city) {
+    query += ' AND LOWER(a.city) LIKE ?';
+    queryParams.push('%' + city.toLowerCase() + '%');
   }
 
   if (dateOfBirthStart && dateOfBirthEnd) {
